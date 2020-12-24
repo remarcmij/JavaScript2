5d0ceecfbc170d96aa9c60f1a040481c2ad7269e
Jim Cramer <remarcmij@gmail.com>
2020-12-24 11:06:07 +0100

    Move report file creation to test-menu.js

--- test-helpers/CustomReporter.js
+++ test-helpers/CustomReporter.js
@@ -1,47 +1,18 @@
-const fs = require('fs').promises;
-const path = require('path');
-
-async function unlink(path) {
-  try {
-    await fs.unlink(path);
-  } catch (_) {
-    // ignore
-  }
-}
-
 class MyCustomReporter {
   onRunComplete(contexts, results) {
-    results.testResults.forEach(async ({ testFilePath, testResults }) => {
-      const baseName = path.basename(testFilePath, '.test.js');
-      const reportDir = path.join(__dirname, '../test-reports');
-
-      const todoFilePath = path.join(reportDir, `${baseName}.todo.md`);
-      await unlink(todoFilePath);
-
-      const passFilePath = path.join(reportDir, `${baseName}.pass.md`);
-      await unlink(passFilePath);
-
-      const failFilePath = path.join(reportDir, `${baseName}.fail.md`);
-      await unlink(failFilePath);
-
+    let report = '';
+    results.testResults.forEach(async ({ testResults }) => {
       const failedTestResults = testResults.filter(
-        testResult => testResult.status === 'failed'
+        (testResult) => testResult.status === 'failed'
       );
 
-      let report;
-
-      if (failedTestResults.length === 0) {
-        report = 'All tests passed';
-        console.error(report);
-        await fs.writeFile(passFilePath, report, 'utf8');
-      } else {
-        report = failedTestResults
-          .map(testResult => testResult.fullName)
+      if (failedTestResults.length !== 0) {
+        report += failedTestResults
+          .map((testResult) => testResult.fullName)
           .join('\n');
-        console.error(report);
-        await fs.writeFile(failFilePath, '```\n' + report + '\n```', 'utf8');
       }
     });
+    console.log(report);
   }
 }
 
--- test-menu.js
+++ test-menu.js
@@ -1,6 +1,37 @@
+const fs = require('fs').promises;
+const path = require('path');
 const inquirer = require('inquirer');
 const { execSync } = require('child_process');
 const menu = require('./test-menu.json');
+
+async function unlink(path) {
+  try {
+    await fs.unlink(path);
+  } catch (_) {
+    // ignore
+  }
+}
+
+async function writeReport(name, report) {
+  const reportDir = path.join(__dirname, './test-reports');
+  const todoFilePath = path.join(reportDir, `${name}.todo.txt`);
+  await unlink(todoFilePath);
+
+  const passFilePath = path.join(reportDir, `${name}.pass.txt`);
+  await unlink(passFilePath);
+
+  const failFilePath = path.join(reportDir, `${name}.fail.txt`);
+  await unlink(failFilePath);
+
+  if (report) {
+    await fs.writeFile(failFilePath, report, 'utf8');
+    return report;
+  }
+
+  const message = 'All tests passed';
+  await fs.writeFile(passFilePath, message, 'utf8');
+  return message;
+}
 
 function selectWeek() {
   return inquirer.prompt([
@@ -14,23 +45,37 @@ function selectWeek() {
 }
 
 async function main() {
+  const { week } = await selectWeek();
+  const exercises = menu[week];
+  const { name } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'name',
+      message: 'Which exercise?',
+      choices: Object.keys(exercises),
+    },
+  ]);
+
+  let report = '';
+  console.log('\nRunning test, please wait...\n');
+
   try {
-    const { week } = await selectWeek();
-    const { exercise } = await inquirer.prompt([
-      {
-        type: 'list',
-        name: 'exercise',
-        message: 'Which exercise?',
-        choices: menu[week],
-      },
-    ]);
-    console.log('Running test, please wait...');
     execSync(
-      `npx jest ${exercise} --silent false --verbose false --reporters="./test-helpers/CustomReporter.js"`
+      `npx jest ${name} --silent false --verbose false --reporters="./test-helpers/CustomReporter.js"`,
+      { encoding: 'utf8' }
     );
-  } catch (_) {
-    console.log('There were errors.');
+  } catch (err) {
+    report += `*** Unit Test Report ***\n\n${err.stdout}`;
   }
+
+  try {
+    execSync(`npx eslint ${exercises[name]}`, { encoding: 'utf8' });
+  } catch (err) {
+    report += `\n*** ESLint Report ***\n${err.stdout}`;
+  }
+
+  const message = await writeReport(name, report);
+  console.log(message);
 }
 
 main();
