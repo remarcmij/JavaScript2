96541e92ca34a88a944426ec2f2189edc4c2e6bb
Jim Cramer <remarcmij@gmail.com>
2020-12-25 11:51:20 +0100

    Extract reusable code to helpers

--- Week1/js-tests/ex1-bookList.test.js
+++ Week1/js-tests/ex1-bookList.test.js
@@ -1,38 +1,12 @@
-const fs = require('fs').promises;
-const path = require('path');
-const { HtmlValidate } = require('html-validate');
-const { getFormatter } = require('html-validate/dist/cli/formatter');
-const { setUp } = require('./helpers');
-
-const stylish = getFormatter('stylish');
-
-const exercisesDir = path.join(__dirname, '../js-exercises/ex1-bookList');
-
-const htmlValidate = new HtmlValidate({
-  extends: ['html-validate:recommended'],
-  rules: { 'no-trailing-whitespace': 'off' },
-});
+const { copyFiles, setUp, validateHTML } = require('./helpers');
 
 describe('Generated HTML', () => {
   beforeAll(async () => {
-    await fs.copyFile(
-      path.join(exercisesDir, 'index.html'),
-      './temp/index.html'
-    );
-    await fs.copyFile(path.join(exercisesDir, 'index.js'), './temp/index.js');
-
+    await copyFiles('ex1-bookList');
     await setUp(page);
   });
 
-  test('should be syntactically valid', async () => {
-    const outerHTML = await page.evaluate(
-      () => document.documentElement.outerHTML
-    );
-    const htmlText = `<!DOCTYPE html>\n${outerHTML}`;
-    const report = htmlValidate.validateString(htmlText);
-    const validationReport = stylish(report);
-    expect(validationReport).toBe('');
-  });
+  test('should be syntactically valid', validateHTML);
 
   test('should contain a <ul> with 3 <li> elements', async () => {
     const result = await page.evaluate(() => {
--- Week1/js-tests/helpers.js
+++ Week1/js-tests/helpers.js
@@ -1,11 +1,23 @@
+const fs = require('fs').promises;
+const path = require('path');
+const { HtmlValidate } = require('html-validate');
+const { getFormatter } = require('html-validate/dist/cli/formatter');
+const stylish = getFormatter('stylish');
+
 const config = {
   baseUrl: 'http://localhost:5000/',
   blockedResourceTypes: ['image', 'stylesheet', 'font'],
 };
 
+async function copyFiles(exerciseDirName) {
+  const exercisesDir = path.join(__dirname, '../js-exercises', exerciseDirName);
+  await fs.copyFile(path.join(exercisesDir, 'index.html'), './temp/index.html');
+  await fs.copyFile(path.join(exercisesDir, 'index.js'), './temp/index.js');
+}
+
 async function setUp(page) {
   await page.setRequestInterception(true);
-  page.on('request', req => {
+  page.on('request', (req) => {
     if (config.blockedResourceTypes.includes(req.resourceType())) {
       req.abort();
     } else {
@@ -15,6 +27,23 @@ async function setUp(page) {
   return page.goto(config.baseUrl, { waitUntil: 'networkidle0' });
 }
 
+const htmlValidate = new HtmlValidate({
+  extends: ['html-validate:recommended'],
+  rules: { 'no-trailing-whitespace': 'off' },
+});
+
+async function validateHTML() {
+  const outerHTML = await page.evaluate(
+    () => document.documentElement.outerHTML
+  );
+  const htmlText = `<!DOCTYPE html>\n${outerHTML}`;
+  const report = htmlValidate.validateString(htmlText);
+  const validationReport = stylish(report);
+  expect(validationReport).toBe('');
+}
+
 module.exports = {
+  copyFiles,
   setUp,
+  validateHTML,
 };
